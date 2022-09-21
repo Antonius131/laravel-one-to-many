4b60c01d85f87eb523bda591fe36dc39cfd5acbb
Antonius131 <antomendetta@gmail.com>
2022-09-21 16:32:05 +0200

    changing author with user_id

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     protected $fillable = [
-        'author',
+        'user_id',
         'title',
         'article_text',
         'date',
