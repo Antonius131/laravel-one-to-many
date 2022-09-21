a68b416c6a1e57edd206b736861aed882345bf45
Antonius131 <antomendetta@gmail.com>
2022-09-21 16:36:10 +0200

    showing user name in show.blade

--- resources/views/admin/Posts/show.blade.php
+++ resources/views/admin/Posts/show.blade.php
@@ -6,7 +6,7 @@
       <div class="col-8 offset-2">
          <div class="row">
             <div class="col-12">
-               <h6>{{ $post->author }}</h6>
+               <h6>{{ $post->user->name }}</h6>
                <h1>{{ $post->title }}</h1>
                <small>{{ $post->date }}</small>
             </div>
