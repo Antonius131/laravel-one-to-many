a084681dcaa38a388144c1341c6ecbb081ea4702
Antonius131 <antomendetta@gmail.com>
2022-09-21 16:25:20 +0200

    populate users table with new data

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Faker\Generator as Faker;
 
 class UserSeeder extends Seeder
 {
@@ -9,8 +12,21 @@ class UserSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        $myUser = new User();
+        $myUser->name = 'Antonius';
+        $myUser->email = 'antomendetta@gmail.com';
+        $myUser->password = Hash::make('1234hello');
+        $myUser->save();
+
+        for ($i=0; $i<8 ; $i++) { 
+            $user = new User();
+            $user->name = $faker->userName();
+            $user->email = $faker->unique()->email();
+            $user->password = Hash::make($faker->password());
+            $user->save();
+        }
+
     }
 }
